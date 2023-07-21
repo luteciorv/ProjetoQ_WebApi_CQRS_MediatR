d8027faf00f54cb8e38892b937068fe4e490f9a3
Luís Felipe <luisrodrigues@bne.com.br>
2023-07-21 17:24:33 -0300

    Criação da interface e da implementação do Student Repository

---
+++ WebApi.Students/Interfaces/IStudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Students.Entities;
+
+namespace Students.Interfaces
+{
+    public interface IStudentRepository
+    {
+        Task<IReadOnlyCollection<Student>> GetAllAsync();
+        Task<Student?> GetByIdAsync(Guid id);
+
+        void Add(Student student);
+        void Update(Student student);
+        void Delete(Student student);
+
+        Task SaveAsync();
+    }
+}
---
+++ WebApi.Students/Repositories/StudentRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Students.Context;
+using Students.Entities;
+using Students.Interfaces;
+
+namespace Students.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public StudentRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IReadOnlyCollection<Student>> GetAllAsync() =>
+            await _dataContext.Students.ToListAsync();
+
+        public async Task<Student?> GetByIdAsync(Guid id) =>
+            await _dataContext.Students.FindAsync(id);
+
+
+        public void Add(Student student) =>
+            _dataContext.Students.Add(student);
+
+        public void Delete(Student student) =>
+            _dataContext.Students.Remove(student);
+
+        public void Update(Student student) =>
+            _dataContext.Students.Update(student);
+
+        public async Task SaveAsync() =>
+            await _dataContext.SaveChangesAsync();
+    }
+}
